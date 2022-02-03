f21f9bb3896cecd2379cd8f70b790a85001f7eeb
Fabián Lastra <flastrao@gmail.com>
2022-02-03 15:19:39 -0300

    feat(team): added view profile button to navigate to the member profile screen

--- lib/src/team/ui/widgets/team_member.dart
+++ lib/src/team/ui/widgets/team_member.dart
@@ -1,4 +1,6 @@
-import 'package:cloker_app/widgets/generic_button.dart';
+import 'package:clocker_app/bloc/app_bloc.dart';
+import 'package:clocker_app/src/team/ui/screens/member_profile.dart';
+import 'package:clocker_app/widgets/generic_button.dart';
 import 'package:flutter/material.dart';
 import 'package:generic_bloc_provider/generic_bloc_provider.dart';
 
@@ -23,58 +25,77 @@ class _TeamMemberState extends State<TeamMember> {
   void onPressed1() {}
 
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    double width = size.width;
-    double height = size.height;
+    final AppBloc _appBloc = BlocProvider.of<AppBloc>(context);
+    double width = _appBloc.getWidth();
+    double height = _appBloc.getHeight();
+
     return Column(
-      children: [
+      children: <Widget>[
         Container(
-          height: size.height * 0.12,
+          height: height * 0.12,
           decoration: BoxDecoration(
-              color: Colors.white,
-              border: Border(
-                  left: BorderSide(
-                      color: Colors.black, width: size.width * 0.05))),
+            color: Colors.white,
+            border: Border(
+              left: BorderSide(
+                color: Colors.black,
+                width: width * 0.05,
+              ),
+            ),
+          ),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
               Container(
-                margin:
-                    EdgeInsets.only(left: width * 0.02, top: height * 0.028),
+                margin: EdgeInsets.only(
+                  left: width * 0.02,
+                  top: height * 0.028,
+                ),
+                height: height * 0.8,
+                width: width * 0.5,
                 alignment: Alignment.centerLeft,
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Text(
                       widget.memberName,
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                      overflow: TextOverflow.ellipsis,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
                     ),
-                    Text(widget.department,
-                        style:
-                            TextStyle(color: Colors.grey[600], fontSize: 14)),
-                    Text(widget.lastSesionData,
-                        style:
-                            TextStyle(color: Colors.grey[600], fontSize: 10)),
+                    Text(
+                      widget.department,
+                      style: TextStyle(
+                        color: Colors.grey[600],
+                        fontSize: 14,
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(top: height * 0.01),
+                      child: Text(
+                        widget.lastSesionData,
+                        style: TextStyle(
+                          color: Colors.grey[600],
+                          fontSize: 10,
+                        ),
+                      ),
+                    ),
                   ],
                 ),
               ),
               Container(
-                margin: EdgeInsets.only(left: width * 0.33),
-                child: GenericButton(
-                  context: context,
-                  textOrIcon: true,
-                  width: width * 0.1,
-                  height: height * 0.04,
-                  buttonText: "ver perfil",
-                  onPressed: () => onPressed1(),
-                  firstColor: Colors.white,
-                  secondColor: Colors.white,
-                  textSize: 10,
-                  textColor: Color(0xFF303030),
-                  shadow: false,
+                margin: EdgeInsets.only(left: width * 0.23),
+                child: InkWell(
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => MemberProfile()));
+                  },
+                  child: const Text("Ver perfil"),
                 ),
-              )
+              ),
             ],
           ),
         ),
